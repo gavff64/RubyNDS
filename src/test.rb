4833355bd1b31e612cb1b4a4bb8ca3ac06d1fae1
gavff <192639589+gavff64@users.noreply.github.com>
2026-08-18 17:27:19 -0400

    color to test.rb

--- src/test.rb
+++ src/test.rb
@@ -1,6 +1,9 @@
-# (the overuse of comments is for my own sake. I don't work with bare metal or even C lol)
+# (the overuse of comments is for my own sake. I don't work with bare metal or even C, bits blah blah lol)
 
-# shift the bits to the left. Shift integer "1" by X positions.
+# & = remove bits we DON'T want
+# | = combine bits we DO want
+# << = move bits into their assigned position
+
 KEY_A      = 1 << 0 # 000000000001 = 1
 KEY_B      = 1 << 1 # 000000000010 = 2
 KEY_SELECT = 1 << 2 # 000000000100 = 4
@@ -25,6 +28,43 @@ module Input
   end
 end
 
+# 16-bit color per pixel aBBBBBGGGGGRRRRR
+# 5 bits per color channel (RRRRR, GGGGG, BBBBB), one opaque (a).
+# 1 bit is 2 possible states, 2 bits is 4 possible states, etc. So 5 bits is 32 possibilities (index 0 - 31).
+# 32 red possibilities, 32 green, 32 blue = 32,768 total RGB combinations
+# Example: 00000 = no red (0) | 10000 = medium red (rounded 16) | 11111 = full red (31)
+
+# Example 2: Brown as 5 bits per channel is (18, 9, 0). So that's "1 00000 01001 10010". Opaque, no blue, 9 green, 18 red.
+# "31" is the mask because each RGB channel is only 5 bits wide, 31 = 11111, the max. So "(r & 31)" means "keep the lowest 5 bits of r"
+# Therefore, "(18 & 31)" = 10010 because 18 = 10010 and the mask keeps all bits the same. The point of this is because it can self-correct
+# if the input color value contains too many bits. Everything past 5 bits is essentially ignored. This is the purpose of the mask on all colors.
+# Green is 9, so do the same bitwise AND operation with the mask, then shift the bits 5 to the left so you're checking the green bits.
+# Same for blue, shift 10 bits to the left.
+# "(1 << 15)" is just checking 0 or 1 for transparency on the last available position.
+# Lastly, "|" bitwise OR combines the RGB bits into a single 16-bit pixel (including the 1/0 transparency)
+
+# So...
+# red:
+# 0 00000 00000 10010
+# green:
+# 0 00000 01001 00000
+# blue:
+# 0 00000 00000 00000
+# opaque:
+# 1 00000 00000 00000
+# Combined = 1 00000 01001 10010 (brown)
+module Color
+  def self.rgb(r, g, b)
+    (r & 31) | ((g & 31) << 5) | ((b & 31) << 10) | (1 << 15)
+  end
+end
+
+RED = Color.rgb(31, 0, 0)
+YELLOW = Color.rgb(31, 31, 0)
+BLUE = Color.rgb(0, 0, 31)
+GREEN = Color.rgb(0, 31, 0)
+GRAY = Color.rgb(24, 24, 24)
+
 def puts(*args)
   args.each do |a|
     print(a.to_s + "\n")
@@ -34,11 +74,11 @@ end
 while System.main_loop?
   Input.update
 
-  puts("A is pressed") if Input.down?(KEY_A)
-  puts("B is pressed") if Input.down?(KEY_B)
-  puts("X is pressed") if Input.down?(KEY_X)
-  puts("Y is pressed") if Input.down?(KEY_Y)
-  puts("START is pressed") if Input.down?(KEY_START)
+  (puts("A is pressed"); Gfx.fill_rect(:top, 10, 20, 50, 30, RED)) if Input.down?(KEY_A)
+  (puts("B is pressed"); Gfx.fill_rect(:top, 10, 20, 50, 30, YELLOW)) if Input.down?(KEY_B)
+  (puts("X is pressed"); Gfx.fill_rect(:top, 10, 20, 50, 30, BLUE)) if Input.down?(KEY_X)
+  (puts("Y is pressed"); Gfx.fill_rect(:top, 10, 20, 50, 30, GREEN)) if Input.down?(KEY_Y)
+  (puts("START is pressed"); Gfx.fill_rect(:top, 10, 20, 50, 30, GRAY)) if Input.down?(KEY_START)
 
   System.vblank
 end
