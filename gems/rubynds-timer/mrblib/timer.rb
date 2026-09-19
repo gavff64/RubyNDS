13aeb7e306b0f9c08273e448ca73c95602c974d4
gavff <192639589+gavff64@users.noreply.github.com>
2026-09-19 18:20:28 -0400

    added FPS helper

--- gems/rubynds-timer/mrblib/timer.rb
+++ gems/rubynds-timer/mrblib/timer.rb
@@ -11,3 +11,30 @@ module Timer
     @start = System.milliseconds
   end
 end
+
+class FPS
+  attr_reader :value
+
+  def initialize
+    @ticks = 0
+    @started = System.milliseconds
+    @value = 0
+  end
+
+  def tick
+    @ticks += 1
+    elapsed = System.milliseconds - @started
+    return if elapsed < 1000
+
+    @value = @ticks * 1000 / elapsed
+    @ticks = 0
+    @started = System.milliseconds
+    print "\r\e[2K#{@value} FPS"
+  end
+
+  def reset
+    @ticks = 0
+    @started = System.milliseconds
+    @value = 0
+  end
+end
